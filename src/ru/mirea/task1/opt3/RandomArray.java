4ef9ff82f2a350701eb2bef5fa66457290d50dd7
NotReptile <igordxtm@yandex.ru>
2022-09-03 12:01:58 +0300

    1 almost 2 - 32 none

--- src/ru/mirea/task1/opt3/RandomArray.java
+++ src/ru/mirea/task1/opt3/RandomArray.java
@@ -1,7 +1,20 @@
 package ru.mirea.task1.opt3;
+import java.util.Scanner;
+import java.util.Random;
 
 public class RandomArray {
     public static void main(String[] args){
-        
+        Random rand = new Random();
+        System.out.println("Enter the length of the array");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        double[] array = new double[n];
+        for(int i=0;i<n;i++){
+            array[i] = Math.random();
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(array[i]);
+        }
+
     }
 }
