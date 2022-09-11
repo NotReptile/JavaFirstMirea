d2da0e4e3ed9249fe25d4b44eea581e296b2bce6
NotReptile <igordxTM@yandex.ru>
2022-09-11 21:07:15 +0300

    1-6 done

--- src/ru/mirea/task7/TestClass.java
+++ src/ru/mirea/task7/TestClass.java
@@ -8,7 +8,7 @@ public class TestClass{
         System.out.println(s1.getPerimeter()); // which version?
         System.out.println(s1.getColor());
         System.out.println(s1.isFilled());
-        System.out.println(s1.getRadius()); //   В классе Shape нету геттера getRadius
+//        System.out.println(s1.getRadius()); //   В классе Shape нету геттера getRadius
         Circle c1 = (Circle)s1; // Downcast back to Circle
         System.out.println(c1);
         System.out.println(c1.getArea());
@@ -16,13 +16,13 @@ public class TestClass{
         System.out.println(c1.getColor());
         System.out.println(c1.isFilled());
         System.out.println(c1.getRadius());
-        Shape s2 = new Shape();//   Невозможно создать объект у абстрактного класса
+//        Shape s2 = new Shape();//   Невозможно создать объект у абстрактного класса
         Shape s3 = new Rectangle(1.0, 2.0, "RED", false); // Upcast
         System.out.println(s3);
         System.out.println(s3.getArea());
         System.out.println(s3.getPerimeter());
         System.out.println(s3.getColor());
-        System.out.println(s3.getLength());//   В классе Shape нету геттера getLength
+//        System.out.println(s3.getLength());//   В классе Shape нету геттера getLength
         Rectangle r1 = (Rectangle)s3; // downcast
         System.out.println(r1);
         System.out.println(r1.getArea());
@@ -32,16 +32,16 @@ public class TestClass{
         System.out.println(s4);
         System.out.println(s4.getArea());
         System.out.println(s4.getColor());
-        System.out.println(s4.getSide());//   В классе Shape нету геттера getSide
-// Take note that we downcast Shape s4 to Rectangle,
-// which is a superclass of Square, instead of Square
+//        System.out.println(s4.getSide());//   В классе Shape нету геттера getSide
+    // Take note that we downcast Shape s4 to Rectangle,
+    // which is a superclass of Square, instead of Square
         Rectangle r2 = (Rectangle)s4;
         System.out.println(r2);
         System.out.println(r2.getArea());
         System.out.println(r2.getColor());
-        System.out.println(r2.getSide());//   В классе Rectangle нету геттера getSide так как это родительский класс Square
+//        System.out.println(r2.getSide());//   В классе Rectangle нету геттера getSide так как это родительский класс Square
         System.out.println(r2.getLength());
-// Downcast Rectangle r2 to Square
+    // Downcast Rectangle r2 to Square
         Square sq1 = (Square)r2;
         System.out.println(sq1);
         System.out.println(sq1.getArea());
